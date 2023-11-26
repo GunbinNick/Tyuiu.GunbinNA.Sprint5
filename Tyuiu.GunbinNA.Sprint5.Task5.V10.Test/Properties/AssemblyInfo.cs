93f226ead7a006644027ef0b1f90f8ad8bdaa542
Tyuiu.GunbinNA@outlook.com <gunbin1005@gmail.com>
2023-11-26 15:58:55 +0500

    Task5

---
+++ Tyuiu.GunbinNA.Sprint5.Task5.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GunbinNA.Sprint5.Task5.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GunbinNA.Sprint5.Task5.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0c33f94-b9f0-4b7c-b3ce-6dc6dd9d0c83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
